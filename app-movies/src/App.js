fe7f2cbed8f846c25eaed437ad1158cd5ca4a998
Kaique <kiaqbs@gmail.com>
2025-02-22 18:50:23 -0300

    react - basics

--- app-movies/src/App.js
+++ app-movies/src/App.js
@@ -1,25 +1,23 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Banner from "./components/Banner";
+import Container from "./components/container";
+import Card from "./components/Card";
+import videos from "./json/db.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, World!
-          Olá, Mundo!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Banner image="home" />
+      <Container>
+        <h1>Geografia</h1>
+        <section className="cards">
+          { videos.map((video) => <Card id={video.id} key={video.id} />) }
+        </section>
+      </Container>
+      <Footer />
+    </>
   );
 }
 
