9b16b08188ae8af40a23f2b95f2a72e2754d9405
Kaique <kiaqbs@gmail.com>
2025-02-28 18:23:10 -0300

    Update index.js

--- app-movies/src/pages/Home/index.js
+++ app-movies/src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Banner from "../../components/Banner";
-import Container from "../../components/Container";
+import Container from "../../../src/components/Container";
 import Card from "../../components/Card";
 import Carousel from "../../components/Carousel";
 import ScrollToTopButton from "../../components/ScrollToTopButton";
@@ -27,17 +27,17 @@ function Home() {
       <Container>
       {loading ? (
         <Loader />
-      ) : (
-        categories.map((category, index) => (
-          <Category category={category}>
-            <Carousel>
-              {filterCategory(index).map((video) => (
-                <Card id={video.id} key={video.id} />
-              ))}
-            </Carousel>
-          </Category>
-        ))
-      )}
+        ) : (
+          categories.map((category, index) => (
+            <Category category={category}>
+              <Carousel>
+                {filterCategory(index).map((video) => (
+                  <Card id={video.id} key={video.id} />
+                ))}
+              </Carousel>
+            </Category>
+          ))
+        )}
       </Container>
       <Footer />
     </>
