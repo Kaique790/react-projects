2c19c9109fc4feeb7182b9d6bf3aac30e53b8efc
Kaique790 <kiaqbs@gmail.com>
2025-03-18 20:00:37 -0300

    Dependencies - project

--- pomodoro-timer/eslint.config.js
+++ pomodoro-timer/eslint.config.js
@@ -1,29 +1,18 @@
-@@ -1,28 +0,0 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tsParser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      '@typescript-eslint': tsPlugin,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      ...tsPlugin.configs.recommended.rules,
+      '@typescript-eslint/ban-ts-comment': 'warn',
     },
   },
-)
+];
