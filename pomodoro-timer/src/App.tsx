ef90ee4808233910be181253898097e61258bcb0
Kaique790 <kiaqbs@gmail.com>
2025-03-18 21:24:59 -0300

    Routes - React Router Dom

--- pomodoro-timer/src/App.tsx
+++ pomodoro-timer/src/App.tsx
@@ -1,16 +1,15 @@
-import { Button } from "./Components/Button";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
--- pomodoro-timer/src/Components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, buttonVariant } from "./Button.styles";
-
-interface ButtonProps {
-  variant?: buttonVariant;
-}
-
-export function Button({ variant = "primary" }: ButtonProps) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>;
-}
