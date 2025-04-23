e86dce2653c79d6ed1700e04e911b929b200aec4
Kaique790 <kiaqbs@gmail.com>
2025-04-22 22:01:46 -0300

    Layouts - in progress

--- src/pages/Home/components/PostDescription/index.tsx
+++ src/pages/Home/components/PostDescription/index.tsx
@@ -1,7 +1,20 @@
 export default function PostDescription() {
   return (
-    <>
-      <h1>POSTEEE</h1>
-    </>
+    <article className="bg-base-post overflow-hidden max-h-65 max-w-[26rem] p-6 rounded-[0.625rem] flex flex-col gap-5">
+      <header className="flex items-center justify-between">
+        <h3 className="text-base-title text-size-xl">
+          JavaScript data types and data structures
+        </h3>
+        <time className="text-base-span text-size-md">Há 1 dia</time>
+      </header>
+
+      <p>
+        Programming languages all have built-in data structures, but these often
+        differ from one language to another. This article attempts to list the
+        built-in data structures available in... JavaScript and what properties
+        they have. These can be used to build other data structures. Wherever
+        possible, comparisons with other languages are drawn.
+      </p>
+    </article>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,4 @@
 import PostDescription from "./components/PostDescription";
-import SearchForm from "./components/SearchForm";
 import { UserInformations } from "./components/UserInformations";
 
 export function Home() {
@@ -7,9 +6,23 @@ export function Home() {
     <main className="mx-auto max-w-4xl ">
       <UserInformations />
 
-      <SearchForm />
-      <section>
-        <PostDescription />
+      <section className="mt-14">
+        <header className="flex w-full justify-between">
+          <h4 className="text-base-title font text-size-lg">Publicações</h4>
+          <span className="text-base-span text-size-md">6 pulicações</span>
+        </header>
+
+        <form className="w-full text-base-text mt-2">
+          <input
+            type="text"
+            placeholder="Buscar postagem"
+            className="bg-base-input w-full px-4 py-3 border-3 border-base-border rounded-md placeholder:text-base-label"
+          />
+        </form>
+
+        <section className="mt-10">
+          <PostDescription />
+        </section>
       </section>
     </main>
   );
