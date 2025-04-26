54c14747baa0706fbff8f452309172b24025e7a6
Kaique790 <kiaqbs@gmail.com>
2025-04-26 20:04:42 -0300

    pizza shop - init
    Config, plugins, completed

---
+++ pizza-shop/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import "./index.css";
+
+export function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button>Enviarrr</Button>
+    </div>
+  );
+}
