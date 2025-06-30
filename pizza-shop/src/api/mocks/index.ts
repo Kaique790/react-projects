8c81c98bb335be4586419b1769fe9143150df9e9
Kaique790 <kiaqbs@gmail.com>
2025-06-30 19:30:00 -0300

    test: add signIn mock

--- pizza-shop/src/api/mocks/index.ts
+++ pizza-shop/src/api/mocks/index.ts
@@ -1,7 +1,8 @@
 import { env } from "@/env";
 import { setupWorker } from "msw/browser";
+import { signInMock } from "./sign-in-mock";
 
-export const moker = setupWorker();
+export const moker = setupWorker(signInMock);
 
 export async function enableMSW() {
   if (env.MODE !== "test") {
