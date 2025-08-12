b664eaea0e970cd30c9100287e1b577d1bce3864
Kaique790 <kiaqbs@gmail.com>
2025-08-12 08:07:04 -0300

    feat(Auth): add prisma adapter

--- ignite-call/src/app/api/auth/[...nextauth]/route.ts
+++ ignite-call/src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@/lib/auth/prisma-adapter";
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(),
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
