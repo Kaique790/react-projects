451daa2c43a44ec923faabd407084494933dd4ec
Kaique790 <kiaqbs@gmail.com>
2025-08-17 15:30:14 -0300

    feat: add prisma adapter

---
+++ ignite-call/src/pages/_app.page.tsx
@@ -0,0 +1,25 @@
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { DefaultSeo } from "next-seo";
+import { globalStyles } from "@/styles/global";
+
+globalStyles();
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <DefaultSeo
+        openGraph={{
+          type: "website",
+          locale: "pt_BR",
+          url: "https://ignite-call.vercel.app",
+          siteName: "Ignite Call",
+        }}
+      />
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+}
---
+++ ignite-call/src/pages/home/index.tsx
@@ -0,0 +1,42 @@
+import { Heading, Text } from "@ignite-ui/react";
+
+import previewImage from "@/assets/app-preview.png.png";
+import Image from "next/image";
+import { ClaimUsernameForm } from "./components/ClaimUsernameForm";
+import { NextSeo } from "next-seo";
+import { Container, Hero, Preview } from "./styles";
+
+export default function Home() {
+  return (
+    <>
+      <NextSeo
+        title="Descomplique sua agenda | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre."
+      />
+
+      <Container>
+        <Hero>
+          <Heading size="4xl" as="h1">
+            Agendamento descomplicado
+          </Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
+
+          <ClaimUsernameForm />
+        </Hero>
+
+        <Preview>
+          <Image
+            src={previewImage}
+            height={400}
+            quality={100}
+            priority
+            alt="Calendário simbolizando aplicação em funcionamento"
+          />
+        </Preview>
+      </Container>
+    </>
+  );
+}
