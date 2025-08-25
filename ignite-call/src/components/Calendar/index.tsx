bc78de965cbafb0a01d1dcf29c8bbd2a81d84151
Kaique790 <kiaqbs@gmail.com>
2025-08-25 19:29:48 -0300

    feat(Calendar): add handle changing month

--- ignite-call/src/components/Calendar/index.tsx
+++ ignite-call/src/components/Calendar/index.tsx
@@ -7,23 +7,44 @@ import {
   CalendarHeader,
   CalendarTitle,
 } from "./styles";
+import dayjs from "dayjs";
 import { getWeekDays } from "@/utils/get-week-days";
+import { useState } from "react";
 
 export function Calendar() {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set("date", 1);
+  });
+
+  function handlePreviousMonth() {
+    const previousMonthDate = currentDate.subtract(1, "month");
+
+    setCurrentDate(previousMonthDate);
+  }
+
+  function handleNextMonth() {
+    const previousMonthDate = currentDate.add(1, "month");
+
+    setCurrentDate(previousMonthDate);
+  }
+
+  const currentMonth = currentDate.format("MMMM");
+  const currentYear = currentDate.format("YYYY");
+
   const shortWeekDays = getWeekDays({ short: true });
 
   return (
     <CalendarConatiner>
       <CalendarHeader>
         <CalendarTitle>
-          Setembro <span>2025</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
 
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviousMonth} title="previou month">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMonth} title="next month">
             <CaretRight />
           </button>
         </CalendarActions>
