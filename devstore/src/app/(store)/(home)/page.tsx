aef078de191d23a1ce52b0a8b45c33ea04113fee
Kaique790 <kiaqbs@gmail.com>
2025-09-08 13:26:36 -0300

    feat: add home layout

--- devstore/src/app/(store)/(home)/page.tsx
+++ devstore/src/app/(store)/(home)/page.tsx
@@ -1,5 +1,67 @@
-export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+import Image from "next/image";
+import Link from "next/link";
 
-  return <h1>Devstore</h1>;
+export default async function Home() {
+  return (
+    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+      <Link
+        href="/"
+        className="group relative col-span-6 row-span-6 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+      >
+        <Image
+          src="/moletom-never-stop-learning-1.png"
+          alt=""
+          width={840}
+          height={840}
+          quality={100}
+          className="transition-transform duration-500 group-hover:scale-105"
+        />
+        <div className="absolute right-28 bottom-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">Moletom Never stop learning</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold text-nowrap">
+            R$ 129
+          </span>
+        </div>
+      </Link>
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+      >
+        <Image
+          src="/camiseta-dowhile-2022.png"
+          alt=""
+          width={400}
+          height={400}
+          quality={100}
+          className="transition-transform duration-500 group-hover:scale-105"
+        />
+
+        <div className="absolute right-10 bottom-10 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">Camiseta dowhile</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold text-nowrap">
+            R$ 129
+          </span>
+        </div>
+      </Link>
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+      >
+        <Image
+          src="/moletom-java.png"
+          alt=""
+          width={400}
+          height={400}
+          quality={100}
+          className="transition-transform duration-500 group-hover:scale-105"
+        />
+        <div className="absolute right-10 bottom-10 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">Moletom Java</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold text-nowrap">
+            R$ 129
+          </span>
+        </div>
+      </Link>
+    </div>
+  );
 }
