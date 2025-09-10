817f791dc80dfff84c82f85dd3679149eb829e93
Kaique790 <kiaqbs@gmail.com>
2025-09-10 15:58:31 -0300

    feat: add data fetch wrapper

---
+++ app-router-fund/src/env.ts
@@ -0,0 +1,16 @@
+import z from "zod";
+
+const envSchema = z.object({
+  NEXT_PUBLIC_API_BASE_URL: z.url(),
+});
+
+const parsedEnv = envSchema.safeParse(process.env);
+
+if (!parsedEnv.success) {
+  const errors = z.treeifyError(parsedEnv.error);
+  console.error("Invalid enviroment variables: ", errors);
+
+  throw new Error("Invalid enviroment variables.");
+}
+
+export const env = parsedEnv.data;
