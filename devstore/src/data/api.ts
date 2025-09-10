d2f07e2512aa93665f518fceaa9f89ddf52c4a63
Kaique790 <kiaqbs@gmail.com>
2025-09-10 16:37:09 -0300

    feat(utils): update api function to return typed response data

--- devstore/src/data/api.ts
+++ devstore/src/data/api.ts
@@ -1,10 +1,13 @@
 import { env } from "@/env";
 
-export function api(path: string, init?: RequestInit) {
+export async function api<T>(path: string, init?: RequestInit) {
   const baseURL = env.NEXT_PUBLIC_API_BASE_URL;
   const prefixURl = "api".concat(path);
 
   const url = new URL(prefixURl, baseURL);
 
-  fetch(url, init);
+  const response = await fetch(url, init);
+  const data: T = await response.json();
+
+  return data;
 }
