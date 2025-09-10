2ed97d0e0a5ecd8562abe0dbcd17844005f4d183
Kaique790 <kiaqbs@gmail.com>
2025-09-10 16:09:30 -0300

    fix: update API URL construction to include 'api' prefix

--- devstore/src/data/api.ts
+++ devstore/src/data/api.ts
@@ -2,7 +2,9 @@ import { env } from "@/env";
 
 export function api(path: string, init?: RequestInit) {
   const baseURL = env.NEXT_PUBLIC_API_BASE_URL;
-  const url = new URL(path, baseURL);
+  const prefixURl = "api".concat(path);
+
+  const url = new URL(prefixURl, baseURL);
 
   fetch(url, init);
 }
